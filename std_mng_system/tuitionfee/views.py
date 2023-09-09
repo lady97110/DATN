164f6842e8159223c13ebc270673cde0120bbd02
NVLuan <lady97110@gmail.com>
2023-09-10 05:42:53 +0700

    09/09

--- std_mng_system/tuitionfee/views.py
+++ std_mng_system/tuitionfee/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
-from login_std.models import profile_std
-from django.contrib.auth.decorators import login_required
-from django.db import models
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
-from course.models import *
-# Create your views here.
+# from django.shortcuts import render
+# from login_std.models import profile_std
+# from django.contrib.auth.decorators import login_required
+# from django.db import models
+# from django.db.models.signals import post_save, post_delete
+# from django.dispatch import receiver
+# from course.models import *
+# # Create your views here.
 
 
 
-@receiver(post_save, sender = Student_ModuleClass)
-@receiver(post_delete, sender = Student_ModuleClass)
-def update_tuitionfee(sender,  instance, **kwargs):
-    std = instance.idStd
+# @receiver(post_save, sender = Student_ModuleClass)
+# @receiver(post_delete, sender = Student_ModuleClass)
+# def update_tuitionfee(sender,  instance, **kwargs):
+#     std = instance.idStd
 
